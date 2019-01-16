a79550c63780688f4cbcfe1997fab7efd7c439c3
Simon Eddy <simon@simoneddy.com.au>
2019-01-16 22:27:58 +1100

    added middleware for localhost cors

--- app/Http/Controllers/ExhibitionController.php
+++ app/Http/Controllers/ExhibitionController.php
@@ -17,6 +17,7 @@ class ExhibitionController extends Controller
         $this->middleware('auth:api', [
             'except' => ['index', 'show']
         ]);
+        $this->middleware('local.cors');
     }
 
     /**
