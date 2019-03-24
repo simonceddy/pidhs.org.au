b70a146da6815e87d43ccb4750a1bf2d2895bd43
Simon Eddy <simon@simoneddy.com.au>
2019-03-25 01:00:18 +1100

    made uploadscontroller invokable

--- app/Http/Controllers/UploadsController.php
+++ app/Http/Controllers/UploadsController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class UploadsController extends Controller
 {
-    //
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
 }
