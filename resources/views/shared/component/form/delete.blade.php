ad2d201c0b79d3481cd1edf908b67bcbf1a0a2f0
Simon Eddy <simon@simoneddy.com.au>
2019-04-12 17:40:44 +1000

    updated todo, removed old commet

--- resources/views/shared/component/form/delete.blade.php
+++ resources/views/shared/component/form/delete.blade.php
@@ -2,7 +2,6 @@
     <form action="{{ $action }}" method="POST">
         @csrf
         @method('DELETE')
-        {{-- fix onclick!!!! --}}
         <button type="submit" class="delete-button" onclick="return confirm('Confirm delete? This action cannot be undone.')">Delete</button>
     </form>
 </div>
