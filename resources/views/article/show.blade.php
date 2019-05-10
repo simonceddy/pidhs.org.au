07fdbdec3d42802e23604f3b484eee72586d62b5
Simon Eddy <simon@simoneddy.com.au>
2019-05-10 15:57:44 +1000

    buttons styling, added empty button components

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 @auth
     <div class="m-2">
-        <a href="{{ route('article.edit', $slug) }}" class="create-button link">Edit Article</a>
+        <a href="{{ route('article.edit', $slug) }}" class="button button-blue">Edit Article</a>
     </div>
 @endauth
 @include('shared.component.htmlarticle')
--- resources/views/exhibition/component/uploadmedia.blade.php
+++ resources/views/exhibition/component/uploadmedia.blade.php
@@ -1,10 +1,10 @@
-<form method="POST" action="{{$action}}" class="form-default" enctype="multipart/form-data" accept-charset="UTF-8">
+<form method="POST" action="{{$action}}" enctype="multipart/form-data" accept-charset="UTF-8" class="flex flex-row justify-between border border-grey p-2">
     @csrf
-    <div class="flex flex-row m-2 items-center">
+    <div class="flex flex-row m-2 items-center flex-1">
         <label for="files" class="mr-2">Section Media</label>
         <input type="file" multiple name="files[]" id="files"  class="form-control form-input flex-1" required />
     </div>
-    <div>
-        <button class="submit-button" type="submit">Upload Images</button>
+    <div class="m-2">
+        <button class="button button-orange" type="submit">Upload Images</button>
     </div>
 </form>
