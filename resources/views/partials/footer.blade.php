f807f1c2bb17d49dcd74fe0e1386dbf80c20b008
Simon Eddy <simon@simoneddy.com.au>
2019-05-11 16:21:55 +1000

    updates to footer links

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,10 +1,10 @@
 <footer class="border-t-blue flex justify-between w-full">
     <div class="text-left m-4">
         <p>Phillip Island and District Historical Society Inc, ABN: 22 720 654 974</p>
-        <p>Site by <a href="https://simoneddy.com.au" rel="norel">Simon Eddy</a> &copy; 2018. <a href="/login">Site Admin</a></p>
+        <p>Site by <a href="https://simoneddy.com.au" rel="external">Simon Eddy</a> &copy; 2018. <a href="/login">Site Admin</a></p>
     </div>
     <div class="text-right m-4">
-        <a href="http://www.fluccs.com.au" title="Fluccs" target="_blank"><img class="float-right" id="banner" src="/storage/banner.gif" alt="Fluccs" width="120" height="60"></a>
+        <a href="https://www.fluccs.com.au" title="Fluccs" target="_blank"><img class="float-right" id="banner" src="/storage/banner.gif" alt="Fluccs" width="120" height="60"></a>
     </div>
 
 </footer>
