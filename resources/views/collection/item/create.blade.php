18a5cdf237ef127dca70a54191d7162783b7bc14
Simon Eddy <simon@simoneddy.com.au>
2019-05-11 20:47:20 +1000

    collection item upload component

--- resources/views/collection/item/create.blade.php
+++ resources/views/collection/item/create.blade.php
@@ -5,67 +5,12 @@
     <h1>Add items to Collection</h1>
     <form method="POST" action="{{route('item.store', $collection)}}" enctype="multipart/form-data" accept-charset="UTF-8" class="form-default flex flex-col">
         @csrf
+        @for ($i = 0; $i < ($count  = $items ?? 5); $i++)
+            @include('shared.component.form.itemupload')
+        @endfor
         <div class="flex flex-col my-4">
-            <div class="form-group">
-            <label for="image-upload">Image file</label>
-            <input type="file" class="form-control-file" name="image-upload[]" id="image-upload" placeholder="Upload image file">
-            </div>
-            <div class="form-group flex items-center flex-row">
-                <label for="caption">
-                    Caption
-                </label>
-                <textarea name="caption[]" id="caption"></textarea>
-            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
         </div>
-        <div class="flex flex-col my-4">
-            <div class="form-group">
-            <label for="image-upload">Image file</label>
-            <input type="file" class="form-control-file" name="image-upload[]" id="image-upload" placeholder="Upload image file">
-            </div>
-            <div class="form-group flex items-center flex-row">
-                <label for="caption">
-                    Caption
-                </label>
-                <textarea name="caption[]" id="caption"></textarea>
-            </div>
-        </div>
-        <div class="flex flex-col my-4">
-            <div class="form-group">
-            <label for="image-upload">Image file</label>
-            <input type="file" class="form-control-file" name="image-upload[]" id="image-upload" placeholder="Upload image file">
-            </div>
-            <div class="form-group flex items-center flex-row">
-                <label for="caption">
-                    Caption
-                </label>
-                <textarea name="caption[]" id="caption"></textarea>
-            </div>
-        </div>
-        <div class="flex flex-col my-4">
-            <div class="form-group">
-            <label for="image-upload">Image file</label>
-            <input type="file" class="form-control-file" name="image-upload[]" id="image-upload" placeholder="Upload image file">
-            </div>
-            <div class="form-group flex items-center flex-row">
-                <label for="caption">
-                    Caption
-                </label>
-                <textarea name="caption[]" id="caption"></textarea>
-            </div>
-        </div>
-        <div class="flex flex-col my-4">
-            <div class="form-group">
-            <label for="image-upload">Image file</label>
-            <input type="file" class="form-control-file" name="image-upload[]" id="image-upload" placeholder="Upload image file">
-            </div>
-            <div class="form-group flex items-center flex-row">
-                <label for="caption">
-                    Caption
-                </label>
-                <textarea name="caption[]" id="caption"></textarea>
-            </div>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
     </form>
 </div>
 @endsection
---
+++ resources/views/shared/component/form/itemupload.blade.php
@@ -0,0 +1,12 @@
+<div class="flex flex-col my-4">
+    <div class="form-group">
+    <label for="image-upload">Image file</label>
+    <input type="file" class="form-control-file" name="image-upload[]" placeholder="Upload image file">
+    </div>
+    <div class="form-group flex items-center flex-row">
+        <label for="caption">
+            Caption
+        </label>
+        <textarea name="caption[]" id="caption"></textarea>
+    </div>
+</div>
