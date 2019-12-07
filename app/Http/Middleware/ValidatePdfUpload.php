6f303d068f5884072032188c70c5bceef65bd52c
Simon Eddy <simon@simoneddy.com.au>
2019-12-07 11:40:18 +1100

    added basic pdf upload middleware - wip

---
+++ app/Http/Middleware/ValidatePdfUpload.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class ValidatePdfUpload
+{
+    /**
+     * Accepted mimetypes
+     *
+     * @var array
+     */
+    protected $mimetype = 'application/pdf';
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $files = $request->file('pdf-upload') ?? $request->file();
+        //dd($files);
+        if (!empty($files)) {
+            $this->checkFiles($files);
+        }
+        return $next($request);
+    }
+
+    private function checkFiles(array $files)
+    {
+        //dd($files);
+        foreach ($files as $file) {
+            if ($file->getMimeType() !== $this->mimetype) {
+                return response('Invalid filetype.')->setStatusCode(500);
+            }
+        }
+    }
+}
